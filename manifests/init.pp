35aeff40620602f08e6adc519eb2fe6f8ec25528
Jeroen van Meeuwen (Ergo Project) <jeroen.van.meeuwen@ergo-project.org>
2011-06-25 15:52:42 +0100

    Update postfix module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,144 @@
 class postfix {
-    @service { "postfix":
-        ensure => running,
-        enable => true,
-        require => Package["postfix"]
-    }
 
-    @package { "postfix":
-        ensure => installed,
+    file { "/etc/postfix/main.cf":
+        owner => "root",
+        group => "root",
+        mode => 644,
+        source => [
+                "puppet://$server/private/$environment/postfix/main.cf.$hostname",
+                "puppet://$server/private/$environment/postfix/main.cf",
+                "puppet://$server/files/postfix/main.cf.$hostname",
+                "puppet://$server/files/postfix/main.cf",
+                "puppet://$server/modules/postfix/main.cf.$hostname",
+                "puppet://$server/modules/postfix/main.cf"
+            ],
+        require => Package["postfix"],
         notify => Service["postfix"]
     }
 
-    @service { "amavisd":
+    file { "/etc/postfix/master.cf":
+        owner => "root",
+        group => "root",
+        mode => 644,
+        source => [
+                "puppet://$server/private/$environment/postfix/master.cf.$hostname",
+                "puppet://$server/private/$environment/postfix/master.cf",
+                "puppet://$server/files/postfix/master.cf.$hostname",
+                "puppet://$server/files/postfix/master.cf",
+                "puppet://$server/modules/postfix/master.cf.$hostname",
+                "puppet://$server/modules/postfix/master.cf"
+            ],
+        require => Package["postfix"],
+        notify => Service["postfix"]
+    }
+
+    package { "postfix":
+        ensure => installed
+    }
+
+    package { "sendmail":
+        ensure => purged,
+        require => Package["postfix"]
+    }
+
+    service { "postfix":
+        ensure => running,
+        enable => true,
+        require => [
+                File["/etc/postfix/main.cf"],
+                File["/etc/postfix/master.cf"],
+                Package["postfix"]
+            ]
+    }
+
+    service { "amavisd":
         ensure => running,
         enable => true
     }
 
-    @package { "amavisd-new":
-        ensure => installed,
-        notify => Service["amavisd"]
+    class amavisd inherits postfix {
+        file { "/etc/amavisd/amavisd.conf":
+            owner => "root",
+            group => "root",
+            mode => 644,
+            source => [
+                    "puppet://$server/private/$environment/postfix/amavisd.conf.$hostname",
+                    "puppet://$server/private/$environment/postfix/amavisd.conf",
+                    "puppet://$server/files/postfix/amavisd.conf.$hostname",
+                    "puppet://$server/files/postfix/amavisd.conf",
+                    "puppet://$server/modules/postfix/amavisd.conf.$hostname",
+                    "puppet://$server/modules/postfix/amavisd.conf"
+                ],
+            require => Package["amavisd-new"],
+            notify => Service["amavisd"]
+        }
+
+        file { "/etc/clamd.d/amavisd.conf":
+            owner => "root",
+            group => "root",
+            mode => 644,
+            source => [
+                    "puppet://$server/private/$environment/postfix/clamd.conf.$hostname",
+                    "puppet://$server/private/$environment/postfix/clamd.conf",
+                    "puppet://$server/files/postfix/clamd.conf.$hostname",
+                    "puppet://$server/files/postfix/clamd.conf",
+                    "puppet://$server/modules/postfix/clamd.conf.$hostname",
+                    "puppet://$server/modules/postfix/clamd.conf"
+                ],
+            require => Package["clamd"],
+            notify => Service["clamd.amavisd"]
+        }
+
+        package { [
+                "amavisd-new",
+                "clamd"
+            ]:
+            ensure => installed
+        }
+
+        service { "amavisd":
+            ensure => running,
+            enable => true,
+            require => [
+                    File["/etc/amavisd/amavisd.conf"],
+                    Package["amavisd"]
+                ]
+        }
+
+        service { "clamd.amavisd":
+            ensure => running,
+            enable => true,
+            require => [
+                    File["/etc/clamd.d/amavisd.conf"],
+                    Package["clamd"]
+                ]
+        }
     }
 
-    class amavisd inherits postfix {
-        realize(Package["postfix"],Service["postfix"])
-        realize(Package["amavisd-new"],Service["amavisd"])
+    class ldap inherits postfix {
+
+        file { "/etc/postfix/ldap/":
+            owner => "root",
+            group => "root",
+            recurse => true,
+            purge => true,
+            force => true,
+            require => Package["postfix"],
+            notify => Service["postfix"],
+            source => [
+                    "puppet://$server/private/$environment/postfix/ldap.$hostname/",
+                    "puppet://$server/private/$environment/postfix/ldap/",
+                    "puppet://$server/files/postfix/ldap.$hostname/",
+                    "puppet://$server/files/postfix/ldap/",
+                    "puppet://$server/modules/postfix/ldap.$hostname/",
+                    "puppet://$server/modules/postfix/ldap/"
+                ]
+        }
+
+        Service["postfix"] {
+            require +> [
+                    File["/etc/postfix/ldap/"]
+                ]
+        }
     }
 }
